9561e8e5c441570edc3b695bab57594cc5863b78
joshuavijay77 <joshv@umich.edu>
2023-12-03 22:20:19 -0500

    luh jaunski

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ import ImageCarousel from './ImageCarousel';
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {AiFillGithub, AiFillLinkedin, AiFillInstagram} from 'react-icons/ai'
 import deved from '../public/gato-removebg-preview.png'
-import bible from '../public/icons8-bible-94.png'
-import dumbell from '../public/icons8-dumbbell-94.png'
-import presentation from '../public/icons8-presentation-94.png'
+import bible from '../public/icons8-bible-100.png'
+import dumbell from '../public/icons8-basketball-64.png'
+import presentation from '../public/icons8-teacher-80.png'
 import insta485 from '../public/insta485.png'
 import neuralNetwork from '../public/neuralNetwork.png'
 import threeD from '../public/threeD.jpeg'
@@ -48,7 +48,7 @@ export default function Home() {
             <h2 className="text-5xl py-2 text-green-900 font-medium md:text-6xl">Joshua Vijayasegar</h2>
             <h3 className="text-2xl py-2 md:text-3xl">Student at UMich studying CSE </h3>
             <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-lg mx-auto'>
-              Hi, my name is Joshua Vijayasegar! I am a junior studying computer science engineering at the University of Michigan Ann Arbor with
+              Hi, I'm Josh! I am a junior studying computer science engineering at the University of Michigan Ann Arbor with
               pretty good grades. I am interested in exploring backend development and machine learning roles. This summer, I will be working in
               New York as a software engineering intern at Capital One. In my free-time I enjoy playing sports, working out, reading, and
               hanging out with friends.
@@ -59,7 +59,7 @@ export default function Home() {
             <a href="https://www.linkedin.com/in/joshua-v-1746b6174/" target="_blank"><AiFillLinkedin/></a>
             <a href="https://www.instagram.com/joshua_vijay/" target="_blank"><AiFillInstagram/></a>
           </div>
-          <div className='shadow-lg relative bg-gradient-to-b from-green-800 rounded-full w-80 h-80 mt-20 mb-10 mx-auto overflow-hidden md:h-96 md:w-96'>
+          <div className='shadow-lg relative bg-gradient-to-b from-green-700 rounded-full w-80 h-80 mt-20 mb-10 mx-auto overflow-hidden md:h-96 md:w-96'>
             <Image src={deved} alt='picture of me holding a cat' layout='fill' objectFit='cover'/>
           </div>
         </section>
@@ -74,32 +74,32 @@ export default function Home() {
             <p className='text-md py-2 leading-8 text-gray-800'>
             </p>
           </div>
-          <div className='lg:flex gap-10 mb-20'>
+          <div className='lg:flex gap-10 mb-20 '>
             <Link href="/athletics">
-                <div className='text-center shadow-lg p-10 rounded-xl my-10 hover:bg-gray-100 transition duration-300 hover:shadow-none mb-10'>
+                <div className='text-center shadow-lg p-10 rounded-xl my-10 hover:bg-gray-200 transition duration-300 hover:shadow-none mb-10'>
                   <Image src={dumbell} alt='design icon' width={100} height={100} className='mx-auto'/>
                   <h3 className='text-lg font-medium pt-8 pb-2'>Athletics</h3>
                   <p className='py-2'>
-                    Athletics have always played a large role in my life and overall happiness. I enjoy a lot of sports,
-                    and I also enjoy just going to the gym or going for a run. Click on this card to learn more.
+                    I love to play sports and work out. My favorite sports are basketball, tennis, and soccer. My current
+                    goals in the gym are to bench 2 plates, squat 3 plates, and deadlift 4 plates.
                   </p>
                 </div>
             </Link>
-            <div className='text-center shadow-lg p-10 rounded-xl my-10 hover:bg-gray-100 transition duration-300 hover:shadow-none'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 hover:bg-gray-200 transition duration-300 hover:shadow-none'>
               <Image src={presentation} alt='code icon' width={100} height={100} className='mx-auto'/>
               <h3 className='text-lg font-medium pt-8 pb-2'>Career</h3>
               <p className='py-2'>
-                What I think I want to do with my life, and how I am preparing for that often changes. Click on
-                this card to view a timeline of how I decided to choose my career.
+                Right now, I want to be a software engineer at a big tech company. Then I want to start my own company. Then I want to become
+                a highschool teacher or missionary.
               </p>
             </div>
 
-            <div className='text-center shadow-lg p-10 rounded-xl my-10 hover:bg-gray-100 transition duration-300 hover:shadow-none'>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10 hover:bg-gray-200 transition duration-300 hover:shadow-none'>
               <Image src={bible} alt='consulting icon' width={100} height={100} className='mx-auto'/>
               <h3 className='text-lg font-medium pt-8 pb-2'>Faith</h3>
               <p className='py-2'>
-                I enjoy reading about and discussing religion, philosophy, history, and science. I enjoy thinking about difficult
-                questions and discussing them with my friends. Click on this link to view a timeline of my changes in life philosophy.
+                I like to read about Christianity and how it fits within philosophy, history, and science. I enjoy thinking about difficult
+                questions and discussing them with my friends.
               </p>
             </div>
           </div>
